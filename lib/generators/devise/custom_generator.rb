37f25ae66249a3ba4efde6d985d3676612438bb5
Chris Wilson <jackparsons93@gmail.com>
2023-08-06 17:45:54 -0500

    added template

--- lib/generators/devise/custom_generator.rb
+++ lib/generators/devise/custom_generator.rb
@@ -5,6 +5,10 @@ module Devise
     class CustomGenerator < Rails::Generators::Base
 
         source_root File.expand_path('templates', __dir__)
+        def copy_model
+
+          template "model.rb", "app/models/model.rb"
+        end
     end
   end
 end
