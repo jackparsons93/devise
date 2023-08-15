019d787a61a1b2aa0c2aea5536ae4912791402af
Chris Wilson <jackparsons93@gmail.com>
2023-08-15 00:42:52 -0500

    changed registration template

--- lib/generators/devise/templates/controllers/registrations_controller.rb
+++ lib/generators/devise/templates/controllers/registrations_controller.rb
@@ -45,13 +45,13 @@ class <%= if scope; scope.pluralize; end %>RegistrationsController < Devise::Reg
     
  
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [<%= options[:attributes].each { |key, value| "Key: #{key}, Value: #{value}" } %>])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [<%= options[:attributes].each { |key, value| ":#{key}, " } %>])
   end
   
  
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:<%= scope %>])
+    devise_parameter_sanitizer.permit(:account_update, keys: [<%= options[:attributes].each { |key, value| ":#{key}, " } %>])
   end
 
   # The path used after sign up.
